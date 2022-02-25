a89253c30f546ee68831013ab6624487915fc066
SeaDve <davecruz48@gmail.com>
2022-02-25 16:18:47 +0800

    Test

--- src/application.py
+++ src/application.py
@@ -31,6 +31,7 @@ class Application(Gtk.Application):
         window = Gtk.Window(transient_for=self.props.active_window, modal=True)
         window.add(image)
         window.show_all()
+        window.connect("key-press-event", lambda win, key: win.close())
         window.fullscreen()
 
     def on_quit(self, action, args):
